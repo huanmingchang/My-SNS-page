5a5bcc3a4abc9396e0b4ef6bb08bc39144c54845
huanmingchang <huanming1988@gmail.com>
2021-07-06 20:41:47 +0800

    feat: project initiation

---
+++ index.js
@@ -0,0 +1,66 @@
+const BASE_URL = 'https://lighthouse-user-api.herokuapp.com'
+const INDEX_URL = BASE_URL + '/api/v1/users/'
+const users = []
+const dataPanel = document.querySelector('#data-panel')
+
+function renderUserList(data) {
+  let htmlContent = ''
+  data.forEach((user) => {
+    htmlContent += `
+        <div class="col-lg-2 col-sm-3">
+          <div class="mb-2">
+            <div class="card rounded-3">
+              <img
+                class="card-img-top"
+                src="${user.avatar}"
+                alt="User avatar" data-bs-toggle="modal" data-bs-target="#user-modal" data-id="${user.id}"
+              />
+              <div class="card-body" style="height: 4rem;">
+                <h6 class="card-title">${user.name} ${user.surname}</h6>
+              </div>
+							<div class="d-flex justify-content-end mx-3 my-2">
+							  <button class="btn btn-outline-danger btn-add-friend" data-bs-id="${user.id}"><i class="far fa-heart"></i></button>
+							</div>
+            </div>
+          </div>
+        </div>
+    `
+  })
+  dataPanel.innerHTML = htmlContent
+}
+
+function renderUserModal(id) {
+  const modalTile = document.querySelector('#user-modal-title')
+  const modalImage = document.querySelector('#user-modal-image')
+  const modalEmail = document.querySelector('#user-modal-email')
+  const modalGender = document.querySelector('#user-modal-gender')
+  const modalAge = document.querySelector('#user-modal-age')
+  const modalBirthday = document.querySelector('#user-modal-birthday')
+  const modalRegion = document.querySelector('#user-modal-region')
+  axios.get(INDEX_URL + id).then((response) => {
+    const data = response.data
+    modalTile.innerText = data.name + ' ' + data.surname
+    modalImage.innerHTML = `
+    <img src="${data.avatar}" alt="avatar" class="img-fluid rounded-circle w-75"/>
+    `
+    modalEmail.innerText = 'Email: ' + data.email
+    modalGender.innerText = 'Gender: ' + data.gender
+    modalAge.innerText = 'Age: ' + data.age
+    modalBirthday.innerText = 'Birthday: ' + data.birthday
+    modalRegion.innerText = 'Region: ' + data.region
+  })
+}
+
+dataPanel.addEventListener('click', function onPanelClick(event) {
+  if (event.target.matches('.card-img-top')) {
+    renderUserModal(Number(event.target.dataset.id))
+  }
+})
+
+axios
+  .get(INDEX_URL)
+  .then((response) => {
+    users.push(...response.data.results)
+    renderUserList(users)
+  })
+  .catch((error) => console.log(error))
